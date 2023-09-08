09d74417737c3f24bd64ad158021e0c6bdb0d230
Jeremy Dyer <jdye64@gmail.com>
2023-09-07 22:05:08 -0400

    updates

--- src/expr/window.rs
+++ src/expr/window.rs
@@ -21,6 +21,7 @@ use datafusion_expr::expr::WindowFunction;
 use pyo3::prelude::*;
 use std::fmt::{self, Display, Formatter};
 
+use crate::common::df_schema::PyDFSchema;
 use crate::errors::{DataFusionError, py_type_err};
 use crate::expr::logical_node::LogicalNode;
 use crate::sql::logical::PyLogicalPlan;
@@ -34,22 +35,16 @@ pub struct PyWindow {
     window: Window,
 }
 
-#[pyclass(name = "WindowFrame", module = "dask_sql", subclass)]
+#[pyclass(name = "WindowFrame", module = "datafusion.expr", subclass)]
 #[derive(Clone)]
 pub struct PyWindowFrame {
     window_frame: WindowFrame,
 }
 
-#[pyclass(name = "WindowFrameBound", module = "dask_sql", subclass)]
+#[pyclass(name = "WindowFrameBound", module = "datafusion.expr", subclass)]
 #[derive(Clone)]
 pub struct PyWindowFrameBound {
     frame_bound: WindowFrameBound,
-}
-
-impl PyWindow {
-    pub fn new(window: Window) -> Self {
-        Self { window }
-    }
 }
 
 impl From<PyWindow> for Window {
@@ -91,14 +86,18 @@ impl Display for PyWindow {
 
 #[pymethods]
 impl PyWindow {
+
+    /// Returns the schema of the Window
+    pub fn schema(&self) -> PyResult<PyDFSchema> {
+        Ok(self.window.schema.as_ref().clone().into())
+    }
+
     /// Returns window expressions
-    #[pyo3(name = "getGroups")]
     pub fn get_window_expr(&self) -> PyResult<Vec<PyExpr>> {
         py_expr_list(&self.window.window_expr)
     }
 
     /// Returns order by columns in a window function expression
-    #[pyo3(name = "getSortExprs")]
     pub fn get_sort_exprs(&self, expr: PyExpr) -> PyResult<Vec<PyExpr>> {
         match expr.expr.unalias() {
             Expr::WindowFunction(WindowFunction { order_by, .. }) => {
@@ -109,7 +108,6 @@ impl PyWindow {
     }
 
     /// Return partition by columns in a window function expression
-    #[pyo3(name = "getPartitionExprs")]
     pub fn get_partition_exprs(&self, expr: PyExpr) -> PyResult<Vec<PyExpr>> {
         match expr.expr.unalias() {
             Expr::WindowFunction(WindowFunction { partition_by, .. }) => {
@@ -120,7 +118,6 @@ impl PyWindow {
     }
 
     /// Return input args for window function
-    #[pyo3(name = "getArgs")]
     pub fn get_args(&self, expr: PyExpr) -> PyResult<Vec<PyExpr>> {
         match expr.expr.unalias() {
             Expr::WindowFunction(WindowFunction { args, .. }) => {
@@ -131,7 +128,6 @@ impl PyWindow {
     }
 
     /// Return window function name
-    #[pyo3(name = "getWindowFuncName")]
     pub fn window_func_name(&self, expr: PyExpr) -> PyResult<String> {
         match expr.expr.unalias() {
             Expr::WindowFunction(WindowFunction { fun, .. }) => Ok(fun.to_string()),
@@ -140,7 +136,6 @@ impl PyWindow {
     }
 
     /// Returns a Pywindow frame for a given window function expression
-    #[pyo3(name = "getWindowFrame")]
     pub fn get_window_frame(&self, expr: PyExpr) -> Option<PyWindowFrame> {
         match expr.expr.unalias() {
             Expr::WindowFunction(WindowFunction { window_frame, .. }) => Some(window_frame.into()),
@@ -160,17 +155,14 @@ fn not_window_function_err(expr: Expr) -> PyErr {
 #[pymethods]
 impl PyWindowFrame {
     /// Returns the window frame units for the bounds
-    #[pyo3(name = "getFrameUnit")]
     pub fn get_frame_units(&self) -> PyResult<String> {
         Ok(self.window_frame.units.to_string())
     }
     /// Returns starting bound
-    #[pyo3(name = "getLowerBound")]
     pub fn get_lower_bound(&self) -> PyResult<PyWindowFrameBound> {
         Ok(self.window_frame.start_bound.clone().into())
     }
     /// Returns end bound
-    #[pyo3(name = "getUpperBound")]
     pub fn get_upper_bound(&self) -> PyResult<PyWindowFrameBound> {
         Ok(self.window_frame.end_bound.clone().into())
     }
@@ -179,24 +171,20 @@ impl PyWindowFrame {
 #[pymethods]
 impl PyWindowFrameBound {
     /// Returns if the frame bound is current row
-    #[pyo3(name = "isCurrentRow")]
     pub fn is_current_row(&self) -> bool {
         matches!(self.frame_bound, WindowFrameBound::CurrentRow)
     }
 
     /// Returns if the frame bound is preceding
-    #[pyo3(name = "isPreceding")]
     pub fn is_preceding(&self) -> bool {
         matches!(self.frame_bound, WindowFrameBound::Preceding(_))
     }
 
     /// Returns if the frame bound is following
-    #[pyo3(name = "isFollowing")]
     pub fn is_following(&self) -> bool {
         matches!(self.frame_bound, WindowFrameBound::Following(_))
     }
     /// Returns the offset of the window frame
-    #[pyo3(name = "getOffset")]
     pub fn get_offset(&self) -> PyResult<Option<u64>> {
         match &self.frame_bound {
             WindowFrameBound::Preceding(val) | WindowFrameBound::Following(val) => match val {
@@ -223,7 +211,6 @@ impl PyWindowFrameBound {
         }
     }
     /// Returns if the frame bound is unbounded
-    #[pyo3(name = "isUnbounded")]
     pub fn is_unbounded(&self) -> PyResult<bool> {
         match &self.frame_bound {
             WindowFrameBound::Preceding(v) | WindowFrameBound::Following(v) => Ok(v.is_null()),
@@ -233,9 +220,11 @@ impl PyWindowFrameBound {
 }
 
 impl LogicalNode for PyWindow {
+
     fn inputs(&self) -> Vec<PyLogicalPlan> {
-        // table scans are leaf nodes and do not have inputs
-        vec![]
+        vec![self.window
+            .input
+            .as_ref().clone().into()]
     }
 
     fn to_variant(&self, py: Python) -> PyResult<PyObject> {
